3dd23aebb04dec031d966ae1bb95fd64fc6f4ffe
Hideo Hattori <hhatto.jp@gmail.com>
2013-10-05 23:33:55 +0900

    not used code

--- examples/similarity.py
+++ examples/similarity.py
@@ -8,9 +8,6 @@ TARGET_FILE2 = os.path.join(BASE_DIR, 'image/lena-affine.jpg')
 CONFIG_FILE = os.path.join(BASE_DIR, 'test.conf')
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
-kvs = None
-with open(os.path.join(DATA_DIR, 'kvs.json')) as fp:
-    kvs = json.load(fp)
 
 db = Otama({'driver': {'name': 'vlad_nodb'}})
 print(db.similarity({'data': open(TARGET_FILE1, 'rb').read()},
