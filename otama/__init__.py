f745cc285c439da4fe1fb398c531a5a08b8ab797
Hideo Hattori <hhatto.jp@gmail.com>
2014-01-04 16:18:24 +0900

    update

--- otama/__init__.py
+++ otama/__init__.py
@@ -1,2 +1,7 @@
-from otama.otama import Otama, __libotama_version__
+import sys
+
+if int(sys.version[0]) >= 3:
+    from otama.otama import Otama, __libotama_version__
+else:
+    from otama import Otama, __libotama_version__
 from ._version import __version__
